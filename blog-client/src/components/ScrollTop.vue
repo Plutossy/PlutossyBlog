c9c23043737d916ec112c827216248efd905349f
Plutossy <pluto_ssy@outlook.com>
2023-11-27 17:39:06 +0800

    用vite5重构一下

---
+++ blog-client/src/components/ScrollTop.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="scroll-top"></div>
+</template>
+
+<script>
+export default {
+  name: 'scroll-top'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- blog-client/src/pages/Home.vue
+++ blog-client/src/pages/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="home-container">
+    <swiper />
     <my-header />
     <router-view></router-view>
+    <scroll-top />
   </div>
 </template>
 
 <script>
 import MyHeader from '../components/MyHeader.vue'
+import Swiper from '../components/Swiper.vue'
+import ScrollTop from '../components/ScrollTop.vue'
 export default {
   components: {
-    MyHeader
+    MyHeader,
+    Swiper,
+    ScrollTop
   }
 }
 </script>
