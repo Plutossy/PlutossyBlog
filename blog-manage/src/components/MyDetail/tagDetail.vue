cdc650270afe9efaec57ac4538852591bb924a8b
Plutossy <pluto_ssy@outlook.com>
2024-02-29 18:54:13 +0800

    完成标签页面的开发

---
+++ blog-manage/src/components/MyDetail/tagDetail.vue
@@ -0,0 +1,129 @@
+<template>
+  <el-dialog v-model="detailVisible" :title="props.dialogTitle">
+    <el-form ref="ruleFormRef" :rules="rules" :model="detailData" label-width="80px">
+      <el-form-item label="标签名称" prop="name">
+        <el-input v-model="detailData.name" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="cancel">取消</el-button>
+        <el-button type="primary" @click="confirm(ruleFormRef)">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { ElMessageBox, ElNotification } from 'element-plus';
+import type { FormInstance, FormRules } from 'element-plus';
+
+const props = defineProps({
+  dialogVisible: {
+    type: Boolean,
+    default: false,
+  },
+  dialogTitle: {
+    type: String,
+    default: '详情',
+  },
+  dialogData: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+let emit = defineEmits(); // 如果用的setup函数则是用 cotext.emit 去使用
+let detailVisible = ref(false);
+
+interface RuleForm {
+  // 表单数据类型
+  name: string;
+}
+const ruleFormRef: any = ref<FormInstance>(); // 表单实例
+let detailData = reactive<RuleForm>({
+  // 表单数据
+  name: '',
+});
+const rules = reactive<FormRules<RuleForm>>({
+  // 表单验证规则
+  name: [{ required: true, message: '请输入标签名称', trigger: 'blur' }],
+});
+
+watch(
+  () => props.dialogVisible,
+  val => {
+    detailVisible.value = val;
+  }
+);
+watch(
+  () => props.dialogData,
+  val => {
+    // detailData.value = val
+    detailData = val as RuleForm;
+  }
+);
+watch(detailVisible, val => {
+  emit('update:dialogVisible', val);
+});
+
+const cancel = () => {
+  detailVisible.value = false;
+};
+
+const confirm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('验证成功!');
+      ElMessageBox.confirm(props.dialogTitle === '编辑' ? '此操作将修改此信息, 是否确定修改?' : '是否确定新增?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        beforeClose: (action, instance, done) => {
+          if (action === 'confirm') {
+            instance.confirmButtonLoading = true;
+            instance.confirmButtonText = 'Loading...';
+            setTimeout(() => {
+              done();
+              instance.confirmButtonLoading = false;
+            }, 300);
+          } else {
+            done();
+          }
+        },
+      })
+        .then(() => {
+          detailVisible.value = false;
+          if (props.dialogTitle === '编辑') {
+            // updateUser(detailData)
+            ElNotification({
+              type: 'success',
+              message: '修改成功!',
+            });
+          } else if (props.dialogTitle === '新增') {
+            // addUser(detailData)
+            ElNotification({
+              type: 'success',
+              message: '新增成功!',
+            });
+          }
+        })
+        .catch(() => {
+          props.dialogTitle === '编辑'
+            ? ElNotification({
+                type: 'info',
+                message: '已取消修改',
+              })
+            : ElNotification({
+                type: 'info',
+                message: '已取消新增',
+              });
+        });
+    } else {
+      console.log('验证失败!', fields);
+    }
+  });
+};
+</script>
+
+<style lang="scss" scoped></style>
--- blog-manage/src/pages/Tag.vue
+++ blog-manage/src/pages/Tag.vue
@@ -2,20 +2,14 @@
  * @Author: Plutossy pluto_ssy@outlook.com
  * @Date: 2024-01-08 19:17:21
  * @LastEditors: Plutossy pluto_ssy@outlook.com
- * @LastEditTime: 2024-01-08 19:34:18
+ * @LastEditTime: 2024-02-29 18:41:04
  * @FilePath: \PlutossyBlog\blog-manage\src\pages\Blog.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div>
-    <h1>标签管理</h1>
-  </div>
+  <router-view />
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style scoped lang="scss"></style>
