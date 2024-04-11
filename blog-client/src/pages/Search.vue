8e0a5c49ac6ff7633ee5a3a06915ae4de956c615
Plutossy <pluto_ssy@outlook.com>
2024-04-11 17:10:09 +0800

    搜索组件优化

--- blog-client/src/pages/Search.vue
+++ blog-client/src/pages/Search.vue
@@ -2,13 +2,13 @@
  * @Author: Plutossy pluto_ssy@outlook.com
  * @Date: 2024-01-05 17:15:02
  * @LastEditors: Plutossy pluto_ssy@outlook.com
- * @LastEditTime: 2024-01-13 18:39:53
+ * @LastEditTime: 2024-04-11 17:09:46
  * @FilePath: \blog-client\src\pages\Search.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script setup lang="ts">
 import Introduction from '../components/introduction/Introduction.vue';
-import Parallax from '../components/parallax/parallax.vue';
+import Parallax from '../components/parallax/Parallax.vue';
 import { useRoute } from 'vue-router';
 import { onMounted, toRaw } from 'vue';
 
@@ -16,9 +16,8 @@ const route = useRoute();
 
 onMounted(() => {
   // 打印
-  console.log('route:', toRaw(route).query)
-})
-
+  console.log('route:', toRaw(route).query);
+});
 </script>
 
 <template>
@@ -27,8 +26,7 @@ onMounted(() => {
     <Introduction />
     <Introduction />
     <Introduction />
-    <el-pagination :page-size="5" :pager-count="5" layout="prev, pager, next" :total="28" prev-text="上一页" next-text="下一页"
-      :hide-on-single-page="true" background />
+    <el-pagination :page-size="5" :pager-count="5" layout="prev, pager, next" :total="28" prev-text="上一页" next-text="下一页" :hide-on-single-page="true" background />
   </div>
 </template>
 
