2f0ef4442eb4bba40c2255a18a9316778e0cb6c3
Plutossy <pluto_ssy@outlook.com>
2024-04-18 09:32:00 +0800

    组件优化

--- blog-manage/src/components/MySearch/MySearch.vue
+++ blog-manage/src/components/MySearch/MySearch.vue
@@ -2,7 +2,7 @@
  * @Author: Plutossy pluto_ssy@outlook.com
  * @Date: 2024-01-22 11:33:53
  * @LastEditors: Plutossy pluto_ssy@outlook.com
- * @LastEditTime: 2024-04-16 17:44:52
+ * @LastEditTime: 2024-04-18 09:31:40
  * @FilePath: \blog-manage\src\components\MySearch\MySearch.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -50,7 +50,6 @@ const props = defineProps({
   },
   goComBack: {
     type: Function,
-    default: () => {},
   },
   showAdd: {
     type: Boolean,
