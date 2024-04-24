b193a73678c9c589e80f3189ed068bb76258d790
Plutossy <pluto_ssy@outlook.com>
2024-04-24 19:08:52 +0800

    优化

--- blog-manage/src/components/MySearch/MySearch.vue
+++ blog-manage/src/components/MySearch/MySearch.vue
@@ -2,7 +2,7 @@
  * @Author: Plutossy pluto_ssy@outlook.com
  * @Date: 2024-01-22 11:33:53
  * @LastEditors: Plutossy pluto_ssy@outlook.com
- * @LastEditTime: 2024-04-24 19:07:27
+ * @LastEditTime: 2024-04-24 19:08:45
  * @FilePath: \blog-manage\src\components\MySearch\MySearch.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -100,8 +100,6 @@ const delAll = () => {
       const ids: string[] = props.multipleSelection.map((item: any) => {
         return item.id + '';
       });
-      // const { code } = props.type === 'user' && (await deleteUsers(ids));
-      // const { code } = props.type === 'type' && (await deleteTypes(ids));
       switch (props.type) {
         case 'user':
           var { code } = await proxy.$apis.user.deleteUsers(ids);
