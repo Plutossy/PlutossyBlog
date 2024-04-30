d2fb1934a3e703367bb5bdf2b0416e3d5f0af9dc
Plutossy <pluto_ssy@outlook.com>
2024-04-30 11:50:06 +0800

    优化

--- blog-manage/src/pages/blog/BlogDetail.vue
+++ blog-manage/src/pages/blog/BlogDetail.vue
@@ -2,7 +2,7 @@
  * @Author: Plutossy pluto_ssy@outlook.com
  * @Date: 2024-03-01 10:19:31
  * @LastEditors: Plutossy pluto_ssy@outlook.com
- * @LastEditTime: 2024-04-30 11:48:25
+ * @LastEditTime: 2024-04-30 11:49:39
  * @FilePath: \blog-manage\src\pages\layout\MyBlogDetail.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -272,7 +272,7 @@ const publishForm2 = async (formEl: FormInstance | undefined, published: number)
   await formEl.validate(async (valid, fields) => {
     if (valid) {
       blogForm.published = published;
-      blogForm.userId = stroe.getters['user/getUserInfo'].id;
+      blogForm.userId = blogForm.userId || stroe.getters['user/getUserInfo'].id;
       if (!blogForm.content) return ElNotification.warning('博客内容不能为空！');
       try {
         const { code } = await proxy.$apis.blog.addBlog(blogForm);
