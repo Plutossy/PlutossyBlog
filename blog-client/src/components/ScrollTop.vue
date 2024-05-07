f409e245530086f2032ef1124796d329ed716837
Plutossy <pluto_ssy@outlook.com>
2024-05-07 13:54:55 +0800

    优化导航条

--- blog-client/src/components/ScrollTop.vue
+++ blog-client/src/components/ScrollTop.vue
@@ -2,7 +2,7 @@
  * @Author: Plutossy pluto_ssy@outlook.com
  * @Date: 2023-11-27 16:59:47
  * @LastEditors: Plutossy pluto_ssy@outlook.com
- * @LastEditTime: 2024-05-07 11:59:45
+ * @LastEditTime: 2024-05-07 13:53:16
  * @FilePath: \blog-client\src\components\ScrollTop.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
